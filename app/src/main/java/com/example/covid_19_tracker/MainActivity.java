b38c21e6b1f33859eadaa4a1a785a940588fef9f
“Chanchai” <fordfanta2541@gmail.com>
2020-05-20 17:14:30 +0700

    [Initial commit] firs commit for import a whole project

---
+++ app/src/main/java/com/example/covid_19_tracker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid_19_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
